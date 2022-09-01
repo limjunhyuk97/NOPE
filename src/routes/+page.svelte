6421ace5366acdca450e374b336f2726bfc072cc
bukim0329 <bukim0329@gmail.com>
2022-09-01 14:01:07 +0900

    [feat]: 회원가입 및 로그인 구현

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,25 @@
 	import { goto } from '$app/navigation';
 
 	import { user } from '$lib/stores';
+	import { supabase } from '$lib/supabase';
 	import { beforeUpdate } from 'svelte';
 
 	beforeUpdate(() => {
-		if (!$user) {
-			goto('/users/login');
-		}
+		if (!$user) goto('/users/login');
 	});
 </script>
 
-<div>메인 페이지</div>
+<div class="px-24 pb-16 mx-auto w-3/5 border rounded flex flex-col gap-4 shadow-2xl relative" />
+<button
+	class="mt-10 w-full border-none py-4"
+	on:click={async () => {
+		const { error } = await supabase.auth.signOut();
+		console.log(error);
+		if (!error) {
+			// $toastMessage = '로그아웃 되었습니다';
+			// goto('/');
+		}
+	}}
+>
+	로그아웃
+</button>
--- src/routes/users/login/+page.svelte
+++ src/routes/users/login/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { goto } from '$app/navigation';
+
 	import { supabase } from '$lib/supabase';
 
 	let loading = false;
@@ -10,7 +12,7 @@
 			loading = true;
 			const { error } = await supabase.auth.signInWithPassword({ email, password });
 			if (error) throw error;
-			alert('Check your email for the login link!');
+			goto('/');
 		} catch (error) {
 			// alert(error.error_description || error.message);
 		} finally {
@@ -19,43 +21,41 @@
 	};
 </script>
 
-<div class="px-24 pb-16 my-32 w-3/5 mx-auto border rounded flex flex-col gap-4 shadow-2xl">
+<div class="px-24 pb-16 mx-auto w-3/5 border rounded flex flex-col gap-4 shadow-2xl relative">
 	<img
-		src="https://tpadnvqhtgwxcexavqic.supabase.co/storage/v1/object/sign/app/NOPE_logo.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhcHAvTk9QRV9sb2dvLnN2ZyIsImlhdCI6MTY2MTk0Nzc2MCwiZXhwIjoxOTc3MzA3NzYwfQ.e2gtZbD8cfwlU4VNSmXdzRFF9ZpBmRACM_jeYx2mtjY"
+		src="https://tpadnvqhtgwxcexavqic.supabase.co/storage/v1/object/sign/app/logo-NOPE.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhcHAvbG9nby1OT1BFLnN2ZyIsImlhdCI6MTY2MjAwMzE4NCwiZXhwIjoxOTc3MzYzMTg0fQ.i3cnkALP8nzLksFCR5c7ZgafzcqjKsqLRfIcRz9XhZU"
+		class="w-2/3 mx-auto"
 		alt="logo"
-		class="pl-24"
 	/>
 	<form
 		on:submit|preventDefault={handleLogin}
-		class="flex flex-col text-2xl gap-8 mt-16"
+		class="flex flex-col text-2xl gap-8 lg:mt-16 lg:px-12 mt-4"
 		action="submit"
 	>
-		<label for="email" class="focus:ring-0 font-bold text-sm font-sans">
-			이메일
-			<input
-				type="email"
-				id="email"
-				class="w-full h-8 border-b border-gray-300 text-sm p-3 mt-2"
-				bind:value={email}
-				required
-			/>
-		</label>
-		<label for="password" class="font-bold text-sm font-sans">
-			비밀번호
-			<input
-				type="password"
-				class="w-full h-8 border-b border-gray-300 text-sm p-3 mt-2"
-				bind:value={password}
-				required
-			/>
-		</label>
-	</form>
-	<div class="w-full justify-end flex text-xs text-gray-600">
-		<a href="/users/signUp" class="flex items-center"> 회원가입 </a>
-	</div>
+		<label for="email" class="focus:ring-0 font-bold text-sm font-sans"> 이메일 </label>
 
-	<div class="flex font-bold justify-evenly my-16">
-		<a class="" href="/users/login">로그인</a>
-		<a href="/users/">아이디/PW 찾기</a>
-	</div>
+		<input
+			type="email"
+			id="email"
+			class="h-8 border-b border-gray-300 text-sm p-3 mt-2"
+			bind:value={email}
+			required
+		/>
+		<label for="password" class="font-bold text-sm font-sans"> 비밀번호 </label>
+		<input
+			type="password"
+			class="h-8 border-b border-gray-300 text-sm p-3 mt-2"
+			bind:value={password}
+			required
+		/>
+
+		<div class="w-full justify-end flex text-xs text-gray-600">
+			<a href="/users/signUp" class="flex items-center"> 회원가입 </a>
+		</div>
+
+		<div class="flex text-sm font-bold justify-evenly my-16 whitespace-nowrap gap-8">
+			<button>로그인</button>
+			<a href="/users/">아이디/PW 찾기</a>
+		</div>
+	</form>
 </div>
