c7d8972f0d39ee1b599000b9da9ee4444b33e352
jun_lim97 <limjunhyuk97@gmail.com>
2022-09-02 16:34:15 +0900

    [Fix]: layout 코드 수정 (?? 적용 간소화)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,6 @@
 	import { beforeUpdate } from 'svelte';
 	import { user } from '$lib/stores';
 	import { supabase } from '$lib/supabase';
-	import { goto } from '$app/navigation';
 
 	beforeUpdate(async () => {
 		const { data } = await supabase.auth.getUser();
@@ -14,21 +13,18 @@
 	});
 
 	supabase.auth.onAuthStateChange((_, session) => {
-		if (session?.user) {
-			$user = session.user;
-		} else {
-			$user = null;
-		}
+		$user = session?.user ?? null;
 	});
 </script>
 
 <div class="w-full bg-gray-100">
-	<div class="h-screen 2xl:w-[1536px] lg:w-[1024px] w-[640px] mx-auto bg-white">
-		<!-- <div
-		class="h-screen 2xl:w-[{DESKTOP_WIDTH}] lg:w-[{TABLET_WIDTH}] w-[{MOBILE_WIDTH}] mx-auto pt-10 bg-white"
-	> -->
+	<div
+		class="h-screen 2xl:w-[{DESKTOP_WIDTH}] lg:w-[{TABLET_WIDTH}] w-[{MOBILE_WIDTH}] mx-auto bg-white"
+	>
 		<slot />
 	</div>
 </div>
+
 <Loading />
+
 <Toast />
