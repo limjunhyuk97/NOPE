4076d325f13da1ccf4e21fed8b63fe8fc425c575
jun_lim97 <limjunhyuk97@gmail.com>
2022-09-02 16:47:45 +0900

    [Fix]: toastMessage writable 변수 형태 수정

--- src/lib/Toast.svelte
+++ src/lib/Toast.svelte
@@ -1,23 +1,27 @@
 <script>
-	import { toastMessage } from '$lib/stores';
-	import { fade } from 'svelte/transition';
+	import { toast } from '$lib/stores';
+	import { scale } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
 
 	$: {
-		if ($toastMessage) {
+		// $toastMessage에 문자열이 들어오고 2초 뒤에 null로 바꿔라
+		if ($toast) {
 			setTimeout(() => {
-				$toastMessage = null;
+				$toast = null;
 			}, 2000);
 		}
 	}
 </script>
 
-{#if $toastMessage}
+{#if $toast}
 	<div
 		on:click={() => {
-			$toastMessage = null;
+			$toast = null;
 		}}
-		transition:fade
+		transition:scale={{ duration: 300, delay: 500, start: 0.5, easing: quintOut }}
+		class="w-screen mx-auto px-3 py-1 fixed right-0 left-0 top-{$toast.top ??
+			20} flex items-center justify-center rounded-3xl opacity-70 bg-blue-500 text-white text-center text-xs z-20"
 	>
-		{$toastMessage}
+		{$toast.message}
 	</div>
 {/if}
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,8 +1,17 @@
 import type { User } from '@supabase/supabase-js';
 import { writable, type Writable } from 'svelte/store';
 
+// User Data
 export const user: Writable<User | null> = writable(null);
 export const profile = writable(null);
+
+// UX
 export const modal = writable(null);
-export const toastMessage: Writable<string | null> = writable('');
 export const loading = writable(false);
+
+// Toast
+type toastType = {
+	message: string;
+	top: number;
+};
+export const toast: Writable<toastType | null> = writable(null);
