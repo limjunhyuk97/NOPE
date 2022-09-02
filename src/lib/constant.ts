63f785ab4ab2a2e98c0b84ed6c9cbb7aaa24462f
jun_lim97 <limjunhyuk97@gmail.com>
2022-09-02 17:32:13 +0900

    [Fix]: 반응형 기준값 숫자화

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,3 +1,8 @@
-export const DESKTOP_WIDTH = '1536px';
-export const TABLET_WIDTH = '1024px';
-export const MOBILE_WIDTH = '640px';
+// responsive style
+export const DESKTOP_WIDTH = 1536;
+export const TABLET_WIDTH = 1024;
+export const MOBILE_WIDTH = 640;
+
+// ICON
+export const ICON =
+	'https://tpadnvqhtgwxcexavqic.supabase.co/storage/v1/object/sign/app/logo-NOPE.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhcHAvbG9nby1OT1BFLnN2ZyIsImlhdCI6MTY2MjEwNTIwMCwiZXhwIjoxOTc3NDY1MjAwfQ.dUwIcvRDzdVtowIOhC4zKg0ZsJz_7M01hk4SLqPIyyQ';
