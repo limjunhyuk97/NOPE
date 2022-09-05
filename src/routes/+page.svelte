14753e73062c615a793fbf7c6d4a8f36bfc04af6
bukim0329 <bukim0329@gmail.com>
2022-09-05 22:34:18 +0900

    [Feat]: 기본 페이지 설정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,8 @@
 <script>
 	import { goto } from '$app/navigation';
-
-	import { user } from '$lib/stores';
-	import { supabase } from '$lib/supabase';
 	import { beforeUpdate } from 'svelte';
 
 	beforeUpdate(() => {
-		if (!$user) goto('/users/signin');
+		goto('/main');
 	});
 </script>
-
-<div class="px-24 pb-16 mx-auto w-3/5 border rounded flex flex-col gap-4 shadow-2xl relative" />
-<button
-	class="mt-10 w-full border-none py-4"
-	on:click={async () => {
-		const { error } = await supabase.auth.signOut();
-		console.log(error);
-		if (!error) {
-			// $toastMessage = '로그아웃 되었습니다';
-			// goto('/');
-		}
-	}}
->
-	로그아웃
-</button>
