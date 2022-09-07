b4e1f0521b0442550e6d2e0d8e24909ca7510b63
jun_lim97 <limjunhyuk97@gmail.com>
2022-09-07 20:19:52 +0900

    [Fix]: 메인 페이지에 존재하는 컴포넌트 생성

--- src/routes/main/components/activity.svelte
+++ src/routes/main/components/activity.svelte
@@ -1,30 +1,72 @@
-<script>
-	export let href = '/main';
+<script lang="ts">
+	export let href = '/';
 	export let imgUrl = '';
 	export let title = '';
 	export let recruiting = true;
-	export let proceeding = false;
 	export let startDate = '';
 	export let endDate = '';
+	export let status = '';
+
+	let w: number;
+
+	type statusDivType = { content: string; color: string };
+	const statusVariation: { [key: string]: statusDivType } = {
+		pending: { content: '활동 시작전', color: 'text-gray-200' },
+		started: { content: '활동 시작됨', color: 'text-green-600' },
+		finished: { content: '활동 종료', color: 'text-red-600' }
+	};
+
+	const recruitingMSG = () => {
+		return recruiting
+			? `<div class="text-blue-600">지원 가능</div>`
+			: `<div class="text-red-600">지원 불가</div>`;
+	};
+
+	const statusMSG = () => {
+		return `<div class="${statusVariation[status].color}">${statusVariation[status].content}</div>`;
+	};
 </script>
 
-<a {href} class="w-1/3 flex flex-col shadow-2xl">
-	<img src={imgUrl} alt={title} class="h-3/4 w-full border-b" />
-	<div class="h-1/4 font-bold flex flex-col px-4 justify-center">
-		<div class="text-start text-lg truncate">{title}</div>
-		<div class="lg:block hidden text-end text-xs gap-1">
-			{#if recruiting}
-				<div class="text-blue-600">지원 가능</div>
-			{:else}
-				<div class="text-red-600">지원 불가</div>
-			{/if}
-			{#if proceeding}
-				<div />
-			{:else}
-				<div>현재 진행중</div>
-			{/if}
-			<div>시작일 {startDate}</div>
-			<div>종료일 {endDate ? endDate : '-'}</div>
+<a {href} class="MAIN-ACTIVITY-WIDTH shadow-2xl" bind:clientWidth={w}>
+	<img src={imgUrl} alt={title} class="w-full h-3/4 object-cover border-b" />
+	<!-- 브라우저 사이즈 -->
+	{#if w >= 340}
+		<div class="h-1/4 flex flex-col px-4 justify-center text-sm">
+			<div class="text-start text-lg truncate">{title}</div>
+			<div class="text-end gap-1">
+				{@html recruitingMSG()}
+				{@html statusMSG()}
+				<div>시작일 {startDate}</div>
+				<div>종료일 {endDate ? endDate : '-'}</div>
+			</div>
 		</div>
-	</div>
+		<!-- 패드 사이즈 -->
+	{:else if w >= 190}<div class="flex flex-col px-4 justify-center ">
+			<div class="text-start text-lg truncate">{title}</div>
+			<div class="lg:block hidden text-end text-xs gap-1">
+				{#if recruiting}
+					<div class="text-blue-600">지원 가능</div>
+				{:else}
+					<div class="text-red-600">지원 불가</div>
+				{/if}
+				<div>시작일 {startDate}</div>
+				<div>종료일 {endDate ? endDate : '-'}</div>
+			</div>
+		</div>
+		<!-- 휴대폰 사이즈 -->
+	{:else}
+		<div class="flex flex-col px-4 justify-center">
+			<div class="text-start text-lg truncate">{title}</div>
+			<div class="lg:block hidden text-end text-xs gap-1">
+				{#if recruiting}
+					<div class="text-blue-600">지원 가능</div>
+				{:else}
+					<div class="text-red-600">지원 불가</div>
+				{/if}
+
+				<div>시작일 {startDate}</div>
+				<div>종료일 {endDate ? endDate : '-'}</div>
+			</div>
+		</div>
+	{/if}
 </a>
