78a2bc33b711efbaf33d2e79f141ed0a28dc28e3
jun_lim97 <limjunhyuk97@gmail.com>
2022-09-07 20:19:46 +0900

    [Fix]: 메인 페이지에 존재하는 컴포넌트 생성

--- src/routes/main/components/button.svelte
+++ src/routes/main/components/button.svelte
@@ -1,6 +1,26 @@
-<script>
-	export let href = '/main';
+<script lang="ts">
 	export let innerText = '이름 없음';
+	let hovering: boolean;
+	let menu = 'all';
+
+	const mouseIn = () => {
+		hovering = true;
+		innerText += ' ';
+	};
+
+	const mouseOut = () => {
+		hovering = false;
+		innerText.trim();
+	};
 </script>
 
-<a {href} class="p-4">{innerText}</a>
+<button
+	class="w-full p-4 flex flex-col text-start gap-4 border-b"
+	on:mouseenter|preventDefault={mouseIn}
+	on:mouseleave|preventDefault={mouseOut}
+>
+	<div class="w-full">
+		{innerText}
+	</div>
+	<slot {hovering} />
+</button>
---
+++ src/routes/main/components/link.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let innerText = '이름 없음';
+	export let href = '/main';
+	export let open = false;
+	export let bgColor = 'white';
+</script>
+
+<a class={open ? bgColor : 'bg-transparent'} {href}>
+	{innerText}
+</a>
