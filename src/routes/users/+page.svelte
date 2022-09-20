6dac291f0ed580c35ac9d1d1da2dfc3429732b54
bukim0329 <bukim0329@gmail.com>
2022-09-21 04:35:11 +0900

    feat: 정보변경 ui 구현

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,19 +1,24 @@
 <script type="ts">
 	import Icon from '$lib/Icon.svelte';
 	import { getImageKey, getSignedUrl } from '$lib/utils';
-	import { fade } from 'svelte/transition';
+	import { fade, fly } from 'svelte/transition';
 	import { editProfileImage } from './+page';
 
 	export let data: any;
 
 	let profileImage: string | null = data.profileImage[0]?.url;
 	let edit = false;
+	let pageState = 'default';
 
 	const fileSelectedHandler = async (event: any) => {
 		const key = await getImageKey(event.target.files[0]);
 		const signedUrl = await getSignedUrl(key);
 		profileImage = signedUrl;
 		editProfileImage(signedUrl);
+	};
+
+	const changePageState = (state: string) => {
+		pageState = state;
 	};
 </script>
 
@@ -39,9 +44,9 @@
 				/>
 				<div class="bg-black rounded-full text-white">
 					{#if profileImage}
-						<img src={profileImage} class="w-24 h-24 rounded-full" alt="프로필사진" />
+						<img src={profileImage} class="w-36 h-36 rounded-full" alt="프로필사진" />
 					{:else}
-						<Icon icon="user" size={96} />
+						<Icon icon="user" size={144} />
 					{/if}
 				</div>
 				<div
@@ -53,9 +58,25 @@
 				</div>
 			</label>
 
-			<button class="flex gap-4">
-				<div>내 정보</div>
-				<Icon icon="settings" />
+			<button
+				on:click={() => {
+					switch (pageState) {
+						case 'default':
+							changePageState('editProfile');
+							break;
+						case 'editProfile':
+							changePageState('default');
+					}
+				}}
+				class="flex gap-4"
+			>
+				{#if pageState === 'editProfile'}
+					<div>활동 관리</div>
+					<Icon icon="tool" />
+				{:else if pageState === 'default'}
+					<div>내 정보</div>
+					<Icon icon="settings" />
+				{/if}
 			</button>
 		</div>
 		<div class="flex justify-between items-end">
@@ -81,12 +102,54 @@
 			<button>+</button>
 		</div>
 	</div>
-	<div class="flex flex-col w-1/2 h-screen pt-28 pl-12 gap-8 text-start">
-		<div class="text-3xl pb-4">활동 관리</div>
-		<button class="text-start text-gray-500 text-xl">활동 개설</button>
-		<button class="text-start text-gray-500 text-xl">주최 활동 관리</button>
-		<button class="text-start text-gray-500 text-xl">참여 활동 관리</button>
-		<button class="text-start text-gray-500 text-xl">산출물 관리</button>
-		<button class="text-start text-gray-500 text-xl">회원 탈퇴</button>
-	</div>
+	{#if pageState === 'default'}
+		<div in:fly={{ x: -64 }} class="flex flex-col w-1/2 h-screen pt-28 pl-12 gap-8 text-start">
+			<div class="text-3xl pb-4">활동 관리</div>
+			<button class="text-start text-gray-500 text-xl">활동 개설</button>
+			<button class="text-start text-gray-500 text-xl">주최 활동 관리</button>
+			<button class="text-start text-gray-500 text-xl">참여 활동 관리</button>
+			<button class="text-start text-gray-500 text-xl">산출물 관리</button>
+			<button class="text-start text-gray-500 text-xl">회원 탈퇴</button>
+		</div>
+	{:else if pageState === 'editProfile'}
+		<div in:fly={{ x: -64 }} class="flex flex-col w-1/2 h-screen pt-28 pl-12 gap-6 text-start">
+			<div class="text-3xl pb-4">내 정보 변경</div>
+			<label for="id" class="text-xl flex justify-between items-center">
+				<span> 아이디</span>
+				<input
+					class="text-end px-4 py-2 bg-gray-100 rounded-full focus:outline-none"
+					type="text"
+					id="id"
+				/>
+			</label>
+			<span class="flex justify-end"><button>아이디 변경</button></span>
+			<label for="beforePassword" class="text-xl flex justify-between items-center">
+				<span> 이전 비밀 번호</span>
+				<input
+					class="text-end px-4 py-2 bg-gray-100 rounded-full focus:outline-none"
+					type="text"
+					id="beforePassword"
+				/>
+			</label>
+			<label for="newPassword" class="text-xl flex justify-between items-center"
+				><span> 새 비밀번호</span>
+				<input
+					class="text-end px-4 py-2 bg-gray-100 rounded-full focus:outline-none"
+					type="text"
+					id="newPassword"
+				/>
+			</label>
+			<label for="passwordCheck" class="text-end text-xl flex justify-between">
+				<span>새 비밀번호 확인</span>
+				<input
+					class="px-4 py-2 bg-gray-100 rounded-full focus:outline-none"
+					type="text"
+					id="passwordCheck"
+				/>
+			</label>
+			<span class="flex justify-end"><button>비밀번호 변경</button></span>
+			<label for="descriptions"> 자기소개 </label>
+			<textarea class="h-32 border-y outline-none" />
+		</div>
+	{/if}
 </div>
