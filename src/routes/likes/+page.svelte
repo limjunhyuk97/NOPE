e912eda201bfdc52cb1f42e40d4422371c7968bc
jun_lim97 <limjunhyuk97@gmail.com>
2022-09-20 15:24:42 +0900

    [Feat]: 찜하기 페이지 구현 완료

---
+++ src/routes/likes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Activities } from '$lib/types/main';
+	import ActivityWrapper from '$lib/components/main/ActivityWrapper.svelte';
+
+	/** @type {import('./$types').PageData} */
+	export let data: any;
+
+	const Likes: ArrayLike<Activities> = data.activities;
+</script>
+
+<!-- Activity 컴포넌트 주입 -->
+<div class="w-full">
+	<!-- all -->
+	<ActivityWrapper Activites={Likes} activities_type_kor={'찜한 활동'} />
+</div>
