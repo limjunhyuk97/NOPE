d819b8f1d9f9d604c35297eee4143fc332cb63ee
jun_lim97 <limjunhyuk97@gmail.com>
2022-09-20 15:26:10 +0900

    [Fix]: 불필요한 type 정의 제거

--- src/routes/users/+page.ts
+++ src/routes/users/+page.ts
@@ -4,10 +4,6 @@ import { user } from '$lib/stores';
 import { supabase } from '$lib/supabase';
 import { get } from 'svelte/store';
 
-export interface ProfileTypes {
-	id: string;
-}
-
 /** @type {import('./$types').PageLoad} */
 export async function load({ parent }) {
 	await parent();
