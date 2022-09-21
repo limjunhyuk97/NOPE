a39232249b265c264375a0f9718280e5c8573727
bukim0329 <bukim0329@gmail.com>
2022-09-22 00:03:37 +0900

    fix: 데이터베이스 테이블 변경에 따른 수정

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -6,20 +6,24 @@
 
 	export let data: any;
 
-	let profileImage: string | null = data.profileImage[0]?.url;
-	let edit = false;
+	console.log(data);
+	let profile = data.profile;
+	let profileImageUrl: string | null = data.profile.images?.url;
+	let profileEdit = false;
 	let pageState = 'default';
 
 	const fileSelectedHandler = async (event: any) => {
 		const key = await getImageKey(event.target.files[0]);
 		const signedUrl = await getSignedUrl(key);
-		profileImage = signedUrl;
-		editProfileImage(signedUrl);
+		profileImageUrl = signedUrl;
+		editProfileImage(profile.image_id, signedUrl);
 	};
 
 	const changePageState = (state: string) => {
 		pageState = state;
 	};
+
+	const changeName = (name: string) => {};
 </script>
 
 <div in:fade class="flex text-xl">
@@ -27,10 +31,10 @@
 		<div class="flex justify-between items-end">
 			<label
 				on:mouseenter={() => {
-					edit = true;
+					profileEdit = true;
 				}}
 				on:mouseleave={() => {
-					edit = false;
+					profileEdit = false;
 				}}
 				for="img_export"
 				class="relative cursor-pointer"
@@ -43,14 +47,18 @@
 					on:change={(e) => fileSelectedHandler(e)}
 				/>
 				<div class="bg-black rounded-full text-white">
-					{#if profileImage}
-						<img src={profileImage} class="w-36 h-36 rounded-full object-cover" alt="프로필사진" />
+					{#if profileImageUrl}
+						<img
+							src={profileImageUrl}
+							class="w-36 h-36 rounded-full object-cover"
+							alt="프로필사진"
+						/>
 					{:else}
 						<Icon icon="user" size={144} />
 					{/if}
 				</div>
 				<div
-					class="{edit
+					class="{profileEdit
 						? ''
 						: 'hidden'} absolute right-0 bottom-0 bg-white rounded-full p-2 border border-gray-800"
 				>
@@ -81,11 +89,11 @@
 		</div>
 		<div class="flex justify-between items-end">
 			<div>이름</div>
-			<div>{data.profile.name}</div>
+			<div>{profile.name}</div>
 		</div>
 		<div class="flex justify-between items-end">
 			<div>이메일</div>
-			<div>{data.profile.email}</div>
+			<div>{profile.email}</div>
 		</div>
 		<div class="flex justify-between items-end">
 			<div>비밀번호</div>
@@ -126,9 +134,12 @@
 					class="text-end px-4 py-2 bg-gray-100 rounded-full focus:outline-none"
 					type="text"
 					id="id"
+					value={profile.name}
 				/>
 			</label>
-			<span class="flex justify-end"><button>이름 변경</button></span>
+			<span class="flex justify-end"
+				><button on:click={(e) => changeName('')}>이름 변경</button></span
+			>
 			<label for="beforePassword" class="text-xl flex justify-between items-center">
 				<span>이전 비밀 번호</span>
 				<input
--- src/routes/users/+page.ts
+++ src/routes/users/+page.ts
@@ -5,24 +5,12 @@ import { user } from '$lib/stores';
 import { supabase } from '$lib/supabase';
 import { get } from 'svelte/store';
 
-export interface ProfileTypes {
-	id: string;
-}
-
 const getProfile = async () => {
 	const { data, error } = await supabase
 		.from('users')
-		.select('*, users_images("*")')
-		.eq('id', get(user)?.id);
-	if (error) {
-		console.log(error);
-	} else {
-		return data[0];
-	}
-};
-
-const getProfileImage = async (id: string) => {
-	const { data, error } = await supabase.from('images').select('*').eq('id', id);
+		.select('email, name, descriptions, image_id, images("url")')
+		.eq('id', get(user)?.id)
+		.single();
 	if (error) {
 		console.log(error);
 	} else {
@@ -30,73 +18,45 @@ const getProfileImage = async (id: string) => {
 	}
 };
 
-const setImage = async (id: string, url: string) => {
-	const { error } = await supabase.from('images').insert([{ id: id, url: url }]);
+const updateUsers = async (data) => {
+	const { error } = await supabase.from('users').update(data).eq('id', get(user)?.id);
 
 	if (error) {
 		console.log(error);
 	}
 };
 
-const setUserImage = async (id: string) => {
-	const { error } = await supabase
-		.from('users_images')
-		.insert([{ id: uuidv4(), user_id: get(user)?.id, image_id: id }]);
-
-	if (error) {
-		console.log(error);
-	}
-};
-
-const deleteImage = async (id: string) => {
-	const { error } = await supabase.from('images').delete().match({ id: id });
-
-	if (error) {
-		console.log(error);
-	}
-};
-
-const deleteUserImage = async (id: string) => {
-	const { error } = await supabase.from('users_images').delete().match({ image_id: id });
-
-	if (error) {
-		console.log(error);
-	}
-};
-
-const deleteBeforeImage = async () => {
+const upsertProfileImage = async (imageId: string, url: string) => {
 	const { data, error } = await supabase
-		.from('users_images')
-		.select('id, user_id, image_id')
-		.eq('user_id', get(user)?.id);
+		.from('images')
+		.upsert({ id: imageId, url: url }, { onConflict: 'id' });
 
 	if (error) {
 		console.log(error);
-	} else if (data[0]) {
-		await deleteUserImage(data[0].image_id);
-		await deleteImage(data[0].image_id);
 	}
 };
 
-export const editProfileImage = async (url: string | null) => {
+export const editProfileImage = async (imageId: string | null, url: string | null) => {
 	if (!url) {
 		return;
 	}
-	const id = uuidv4();
+	if (!imageId) {
+		imageId = uuidv4();
+	}
 
-	await deleteBeforeImage();
-	await setImage(id, url);
-	await setUserImage(id);
+	//images 테이블 행 upsert
+	await upsertProfileImage(imageId, url);
+
+	//users의 image_id가 null일 때는 처음 상태이므로 처음에만 실행
+	if (!imageId) {
+		await updateUsers({ image_id: imageId });
+	}
 };
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ parent }) {
 	await parent();
 	const profile = await getProfile();
-	let profileImage: any = {};
-	if (profile?.users_images.length > 0) {
-		profileImage = await getProfileImage(profile?.users_images[0]?.image_id);
-	}
 
-	return { profile, profileImage };
+	return { profile };
 }
