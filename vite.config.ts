c23949b1c108d85a037b8b49aa2485ac1795cb63
jun_lim97 <limjunhyuk97@gmail.com>
2022-09-22 16:45:23 +0900

    [Fix]: CommonJS dynamic importing 안되는 문제 해결중

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import dynamicImport from 'vite-plugin-dynamic-import';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [
-		sveltekit(),
-		dynamicImport({
-			onFiles: (files) => files.filter((f) => !f.includes('codemirror.js'))
-		})
-	]
+	plugins: [sveltekit()]
 };
 
 export default config;
