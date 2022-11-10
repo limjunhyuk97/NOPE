34af4cafaec2f9785505b9f3654971b1488b8879
jun_lim97 <limjunhyuk97@gmail.com>
2022-11-11 00:17:47 +0900

    [Feat]: 모달 기능 구현 중

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,12 @@
+<script>
+	$: {
+	}
+</script>
+
+{#if false}
+	<div class="absolute top-0 w-full h-full bg-black opacity-40 z-10" />
+
+	<div class="absolute top-0 flex justify-center items-center  w-full h-full z-10 ">
+		<div class="flex flex-col max-w-md px-12 py-8 bg-white " />
+	</div>
+{/if}
