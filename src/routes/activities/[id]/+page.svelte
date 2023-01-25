f31d2548709044f8d25d10068214c0aa50d34cb6
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-01-26 01:24:07 +0900

    [feat]: 활동 상세 페이지 구현 완료

--- src/routes/activities/[id]/+page.svelte
+++ src/routes/activities/[id]/+page.svelte
@@ -1,43 +1,92 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import type { ActivityCard } from '$lib/types/activities';
+	import Icon from '$lib/Icon.svelte';
 	import { USER_STATUS } from '$lib/constants';
+	import { user, toast } from '$lib/stores';
+	import { getCommentData, writeComment } from './+page';
+	import Comment from '$lib/components/comments/Comment.svelte';
 	import { Viewer } from 'bytemd';
+
 	/** @type {import('./$types').PageData} */
 	export let data: any;
 
-	const activityData = data.activityData[0];
-	const comments = activityData.comments;
-	const userStatus = Symbol.for(data.userStatus) ?? USER_STATUS.LOGOUT;
+	const activityData: ActivityCard = data.activityData;
+	const activityImage = data.activityImage;
+	const userStatus = data?.userStatus ? USER_STATUS.LOGOUT : USER_STATUS.LOGOUT;
 
-	console.log(activityData);
+	let comments = data.comments;
+
+	// reload comments
+	const reloadComments = async () => {
+		comments = await getCommentData(activityData.id);
+	};
+
+	let myComment = '';
+
+	// write comment
+	const writCommentHandler = async () => {
+		if (myComment === '') {
+			$toast = '내용을 입력해주세요!';
+			return;
+		}
+		await writeComment($user?.id, activityData.id, myComment);
+		await reloadComments();
+	};
 </script>
 
-<div class=" flex w-full h-full">
-	<div class="relative flex flex-col w-full h-full pt-24 p-12">
-		{#if activityData?.images?.url}
+<div class="relative flex w-full h-screen">
+	<div class="relative w-3/4 h-full overflow-y-auto scrollbar-hide">
+		{#if activityImage}
 			<img
-				class="absolute top-0 left-0 w-full h-32 object-cover opacity-40"
-				src={activityData?.images?.url}
+				class="top-0 left-0 w-full h-40 object-cover"
+				src={activityImage}
 				alt={activityData.title}
 			/>
 		{:else}
-			<div class="absolute top-0 left-0 w-full h-32 bg-gray-400 opacity-40" />
+			<div class="top-0 left-0 w-full h-40 bg-gray-400 opacity-40" />
 		{/if}
-		<h1 class="w-fit p-2 text-2xl z-10 bg-white rounded">{activityData.title}</h1>
-		<div class="min-h-[80%] mt-6 md-body"><Viewer value={activityData.details} /></div>
-		<div class="flex gap-12">
-			{#if userStatus === USER_STATUS.NOTAPPLIED}
-				<button class="text-green-700 ">참여 신청하기</button>
-				<button>세션 둘러보기</button>
-			{:else if userStatus === USER_STATUS.APPLIED}
-				<button class="text-green-700">참여 요청완료</button>
-				<button>세션 둘러보기</button>
-			{:else if userStatus === USER_STATUS.LOGOUT}
-				<button>세션 둘러보기</button>
-			{:else}
-				<button class="text-green-700">세션 목록으로</button>
-			{/if}
+		<div class="absolute top-16 p-12">
+			<h1 class="w-fit p-5 text-3xl bg-white rounded-t">{activityData.title}</h1>
+			<div class="min-h-[80%] md-body bg-white"><Viewer value={activityData.details} /></div>
+			<div class="flex gap-12 text-xl bg-white">
+				{#if userStatus === USER_STATUS.NOTAPPLIED}
+					<button class="text-green-700 hover:scale-110 duration-300">참여 신청하기</button>
+					<button class="hover:scale-110 duration-300">세션 둘러보기</button>
+				{:else if userStatus === USER_STATUS.APPLIED}
+					<button class="text-green-700 hover:scale-110 duration-300">참여 요청완료</button>
+					<button class="hover:scale-110 duration-300">세션 둘러보기</button>
+				{:else if userStatus === USER_STATUS.LOGOUT}
+					<button class="hover:scale-110 duration-300">세션 둘러보기</button>
+				{:else}
+					<button class="text-green-700 hover:scale-110 duration-300">세션 목록으로</button>
+				{/if}
+			</div>
 		</div>
 	</div>
-	<div class="w-80 pt-24 p-12 border border-l" />
+	<div class="w-1/4 h-screen border border-l overflow-y-auto scrollbar-hide">
+		<h1 class="sticky top-0 flex items-center h-16 px-6 text-xl bg-white">댓글</h1>
+		<div class="flex flex-col gap-6 w-full min-h-full py-3 ">
+			{#if comments.length > 0}
+				{#each comments as comment}
+					<Comment {comment} on:reload={reloadComments} />
+				{/each}
+			{:else}
+				<div class="px-6 py-3">댓글이 없습니다!</div>
+			{/if}
+		</div>
+		{#if $user}
+			<form
+				class="sticky bottom-0 flex items-center justify-center w-full h-16 z-10 border-t"
+				on:submit|preventDefault={writCommentHandler}
+			>
+				<input
+					type="text"
+					placeholder="댓글을 입력해주세요!"
+					class="flex items-center w-11/12 p-2 border rounded-2xl focus:outline-none"
+					bind:value={myComment}
+				/>
+				<div class="absolute right-4"><Icon icon="chevron-right" size={18} fill="gray" /></div>
+			</form>
+		{/if}
+	</div>
 </div>
--- src/routes/activities/[id]/+page.ts
+++ src/routes/activities/[id]/+page.ts
@@ -3,35 +3,52 @@ import { supabase } from '$lib/supabase';
 import { user } from '$lib/stores';
 import { get } from 'svelte/store';
 import { USER_STATUS } from '$lib/constants';
+import { getSignedUrl } from '$lib/utils';
 import axios from 'axios';
 
 const getActivityData = async (id: string) => {
 	const { data, error } = await supabase
 		.from('activities')
-		.select(
-			'*, comments(users(name), contents, created_at), activity_types("type", "type_kor"), images("url")'
-		)
+		.select('*, activity_types("type", "type_kor"), images("storage_id")')
 		.match({ id });
+	return error ? [] : data[0];
+};
+
+export const getCommentData = async (id: string) => {
+	const { data, error } = await supabase
+		.from('comments')
+		.select('id, users(name, id, images("storage_id")), contents, created_at')
+		.eq('activity_id', id)
+		.order('created_at');
 	return error ? [] : data;
 };
 
-const getUserStatus = async (userID: string | undefined, activityID: string | null) => {
+const getUserStatus = async (user_id: string | undefined, activity_id: string | null) => {
 	try {
 		const status = await axios({
 			method: 'post',
-			url: `/activities/${activityID}`,
-			data: { userID, activityID }
+			url: `/activities/${activity_id}`,
+			data: { userID: user_id, activityID: activity_id }
 		});
-		return status;
+		return status.data;
 	} catch (err) {
 		return USER_STATUS.LOGOUT;
 	}
 };
 
+export const writeComment = async (user_id: string, activity_id: string, contents: string) => {
+	const { data, error } = await supabase
+		.from('comments')
+		.insert({ activity_id, user_id, contents });
+};
+
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, parent }) {
 	await parent();
-	const activityData = await getActivityData(params.id);
-	const userStatus = await getUserStatus(get(user)?.id, activityData[0]?.id);
-	return { activityData, userStatus: userStatus.data };
+	const activityData: ActivityCard = await getActivityData(params.id);
+	const userStatus = await getUserStatus(get(user)?.id, params.id);
+	const activityImage = await getSignedUrl(activityData.images?.storage_id);
+	const comments = await getCommentData(params.id);
+
+	return { activityData, userStatus: userStatus, activityImage, comments };
 }
