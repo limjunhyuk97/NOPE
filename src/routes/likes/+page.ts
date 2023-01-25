829b6a95191f608ec5420eb8fe618ae25a2abc74
jun_lim97 <limjunhyuk97@gmail.com>
2023-01-26 08:14:21 +0900

    [feat]: 불필요한 코드 제거

--- src/routes/likes/+page.ts
+++ src/routes/likes/+page.ts
@@ -5,7 +5,7 @@ import { get } from 'svelte/store';
 const getLikes = async () => {
 	const { data, error } = await supabase
 		.from('activity_likes')
-		.select('activity_id, activities(*, images("storage_id"))')
+		.select('activity_id, activities(*, images(url))')
 		.match({ user_id: get(user)?.id });
 	return error ? [] : data;
 };
