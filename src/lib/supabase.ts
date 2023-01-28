ee2e1465354459016a653e1bac856bcb47cba445
jun_lim97 <limjunhyuk97@gmail.com>
2023-01-28 17:42:24 +0900

    [feat] access-token이 필요한 경우 사용할 supabase 객체 생성

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,3 +4,17 @@ export const supabase = createClient(
 	import.meta.env.VITE_PUBLIC_SUPABASE_URL,
 	import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY
 );
+
+export const supabaseWithToken = (bearer: string) => {
+	const options = {
+		global: {
+			headers: { Authorization: `Bearer ${bearer}` }
+		}
+	};
+
+	return createClient(
+		import.meta.env.VITE_PUBLIC_SUPABASE_URL,
+		import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY,
+		options
+	);
+};
