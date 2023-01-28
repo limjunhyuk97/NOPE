6e795a91e8d82d10527543cc2fe03e1204c354a1
jun_lim97 <limjunhyuk97@gmail.com>
2023-01-28 17:44:54 +0900

    [feat] users 데이터 정책 고려한 개인 정보 수정 기능 구현

---
+++ src/routes/users/+server.ts
@@ -0,0 +1,23 @@
+import type { RequestHandler } from './$types';
+import { checkNameDuplication } from './+page';
+import { supabaseWithToken } from '$lib/supabase';
+
+//** POST : checking email, name value */
+/** @type {import('./$types').RequestHandler} */
+export const POST: RequestHandler = async ({ request }: any) => {
+	const { name, descriptions, id } = await request.json();
+	const bearer = request.headers.get('bearer');
+
+	const duplicated = await checkNameDuplication(name, id);
+
+	if (duplicated) return new Response('중복된 이름이 있습니다', { status: 400 });
+
+	const result = await supabaseWithToken(bearer)
+		.from('users')
+		.update({ name, descriptions })
+		.eq('id', id);
+
+	return new Response(result.status === 204 ? '' : '네트워크 오류', {
+		status: result.status === 204 ? 200 : 400
+	});
+};
