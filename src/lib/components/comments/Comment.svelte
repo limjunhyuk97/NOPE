02fca80594d8d6ba5186f6cdb3715cc48edcd3c8
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-01-31 18:27:57 +0900

    [ing]: 상대방 정보 보여주는 말풍선 만드는 중

--- src/lib/components/comments/Comment.svelte
+++ src/lib/components/comments/Comment.svelte
@@ -2,12 +2,11 @@
 	import type { Comment } from '$lib/types/activities';
 	import { handleDelete } from './Comment';
 	import Icon from '$lib/Icon.svelte';
-	import UserInfo from './UserInfo.svelte';
 	import { getSignedUrl } from '$lib/utils';
 	import { user, toast } from '$lib/stores';
-	import moment from 'moment/min/moment-with-locales';
 	import { createEventDispatcher } from 'svelte';
-	import type { Mouse } from '@playwright/test';
+	import moment from 'moment/min/moment-with-locales';
+
 	moment.locale('ko');
 
 	export let comment: Comment;
@@ -23,29 +22,10 @@
 		reloadComment('reload');
 		$toast = result ? '삭제 되었습니다.' : '삭제 오류';
 	};
-
-	// 타 사용자 정보 모달
-	let showUserDetails = false;
-	$: mousePosition = { x: 0, y: 0 };
-	const handleMousemove = (event: MouseEvent) => {
-		mousePosition.x = event.clientX;
-		mousePosition.y = event.clientY;
-	};
-	const mouseenterHandler = (event: MouseEvent) => {
-		showUserDetails = true;
-		handleMousemove(event);
-	};
-	const mouseleaveHandler = (event: MouseEvent) => {
-		showUserDetails = false;
-	};
 </script>
 
-<div class="flex flex-col gap-2 w-full px-6">
-	<div
-		class="flex items-center gap-4 w-full"
-		on:mouseenter={mouseenterHandler}
-		on:mouseleave={mouseleaveHandler}
-	>
+<div class="relative flex flex-col gap-2 w-full px-6">
+	<div class="flex items-center gap-4 w-full">
 		{#await getSignedUrl(comment.users.images?.storage_id)}
 			<div class="w-9 h-9 p-1 rounded-full bg-gray-300" />
 		{:then image}
@@ -76,7 +56,3 @@
 		</div>
 	</div>
 </div>
-
-{#if showUserDetails}
-	<UserInfo users={comment.users} position={mousePosition} />
-{/if}
