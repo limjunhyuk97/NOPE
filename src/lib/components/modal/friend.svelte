dc86b75e964f645ff32d2c83e46f8c84edce3df1
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-02-04 13:03:04 +0900

    [css]: 관심 기술 스택 뱃지 들어가는 영역 크기 조절

--- src/lib/components/modal/friend.svelte
+++ src/lib/components/modal/friend.svelte
@@ -1,12 +1,15 @@
 <script>
+	// @ts-nocheck
+
 	import Icon from '$lib/Icon.svelte';
 	import { modalData } from '$lib/stores';
 	import { deleteModal } from '$lib/utils';
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="relative flex flex-col p-4 pr-16 bg-white rounded-xl" on:click|stopPropagation>
 	<button class="absolute right-4" on:click={deleteModal}><Icon icon="x" /></button>
-	<div class="flex flex-col gap-4">
+	<div class="flex flex-col gap-4 w-[440px]">
 		<div class="flex gap-8">
 			{#if $modalData?.imageUrl}
 				<img src={$modalData?.imageUrl} class="w-40 h-40 object-cover rounded-xl" />
@@ -17,15 +20,15 @@
 			{/if}
 			<div class="flex flex-col gap-2">
 				<h1 class="text-lg font-bold">{$modalData?.name}</h1>
-				<p class="w-64 h-28 overflow-y-auto scrollbar-hide">
-					{$modalData?.descriptions ? $modalData?.descriptions : '설명이 없습니다!'}
+				<p class="w-64 h-28 overflow-y-auto">
+					{$modalData?.descriptions ? $modalData?.descriptions : '아직 소개가 없습니다!'}
 				</p>
 			</div>
 		</div>
 		{#if $modalData?.user_stacks?.length > 0}
-			<div class="relative grid grid-cols-4 gap-2">
+			<div class="relative flex flex-wrap gap-2 w-full">
 				{#each $modalData?.user_stacks as stack}
-					<img src={stack.stacks.url} alt={stack.stacks.name} class="w-24 h-6" />
+					<img src={stack.stacks.url} alt={stack.stacks.name} />
 				{/each}
 			</div>
 		{/if}
