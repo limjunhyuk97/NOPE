41e7345e2e2913fdede0d2e19daf10efd64ed808
jun_lim97 <limjunhyuk97@gmail.com>
2023-02-28 23:07:11 +0900

    [feat]: 게시글 접근 위한 사이드 바 메뉴 생성

--- src/lib/components/sidebar/Articles.svelte
+++ src/lib/components/sidebar/Articles.svelte
@@ -1,1 +1,1 @@
-<a href="/articles" class="border-b p-4"> 노하우 공유 </a>
+<a href="/articles" class="border-b p-4"> 게시글 </a>
--- src/lib/components/sidebar/UserBtn.svelte
+++ src/lib/components/sidebar/UserBtn.svelte
@@ -2,7 +2,6 @@
 	import { user } from '$lib/stores';
 	import { goto } from '$app/navigation';
 	import { slide, fly } from 'svelte/transition';
-	import { page } from '$app/stores';
 
 	let hovering = false;
 	const mouseEnter = () => {
@@ -14,25 +13,14 @@
 </script>
 
 {#if $user}
-	{#if $page.routeId === 'users/mypage'}
-		<button
-			on:click|preventDefault={() => {
-				goto('/users');
-			}}
-			class="border-b p-4 text-left"
-		>
-			<div in:fly={{ x: -64 }} class="pl-32">마이페이지</div>
-		</button>
-	{:else}
-		<button
-			on:click|preventDefault={() => {
-				goto('/users');
-			}}
-			class="border-b p-4 text-left"
-		>
-			<div in:fly={{ x: 64 }}>마이페이지</div>
-		</button>
-	{/if}
+	<button
+		on:click|preventDefault={() => {
+			goto('/users');
+		}}
+		class="border-b p-4 text-left"
+	>
+		<div in:fly={{ x: 64 }}>마이페이지</div>
+	</button>
 {:else}
 	<a href="/" class="border-b p-4 text-left" on:mouseenter={mouseEnter} on:mouseleave={mouseOut}>
 		함께하기
