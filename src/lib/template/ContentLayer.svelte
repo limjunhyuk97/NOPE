62abe5a2fa284d7916d43830ea2a63f01f0d0cd6
jun_lim97 <limjunhyuk97@gmail.com>
2023-03-01 22:30:27 +0900

    [css]: 마이페이지 컴텐츠 들어가는 부분 레이아웃 변경

--- src/lib/template/ContentLayer.svelte
+++ src/lib/template/ContentLayer.svelte
@@ -5,7 +5,7 @@
 	export let hasBack = false;
 </script>
 
-<div class="w-full h-full">
+<div class="w-full">
 	<div class="flex flex-col gap-4 my-8">
 		<div class="flex items-center gap-4">
 			{#if hasBack}
@@ -14,7 +14,7 @@
 			<h1 class="h-7 text-2xl font-bold">{title}</h1>
 		</div>
 		{#if subtitle.length > 0}
-			<h2 class="mb-4 text-xl font-semibold">{subtitle}</h2>
+			<h2 class="mb-4 text-lg font-semibold">{subtitle}</h2>
 		{/if}
 	</div>
 	<slot />
