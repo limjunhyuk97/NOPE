0e5ba7b2bb2dc34f19a9f1e34a3c8a4f52f278e8
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-03-02 11:37:22 +0900

    [feat]: pending, quit, denied 유저 무시

--- src/routes/users/activities/+page.ts
+++ src/routes/users/activities/+page.ts
@@ -2,6 +2,8 @@ import { user } from '$lib/stores';
 import { get } from 'svelte/store';
 import { supabase } from '$lib/supabase';
 
+const exceptedStatus = ['pending', 'denied', 'quit'];
+
 const _getParticipatingActivities = async () => {
 	const user_id = get(user)?.id;
 	const { data, error } = await supabase
@@ -10,7 +12,7 @@ const _getParticipatingActivities = async () => {
 		.eq('user_id', user_id);
 
 	if (error) return null;
-	return data.filter((activity) => activity.status !== 'pending');
+	return data.filter((activity) => !exceptedStatus.some((status) => status === activity.status));
 };
 
 /** @type {import('./$types').PageLoad} */
