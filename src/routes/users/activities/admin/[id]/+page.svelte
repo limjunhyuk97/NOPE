4a5633846c2992f6c4ae6e36c3b6e9ce80aea1be
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-03-02 12:01:48 +0900

    [feat]: 관리 활동 상세 페이지 생성

--- src/routes/users/activities/admin/[id]/+page.svelte
+++ src/routes/users/activities/admin/[id]/+page.svelte
@@ -1,1 +1,23 @@
-hello
+<script>
+	import ContentLayer from '$lib/template/ContentLayer.svelte';
+	import { mypageSidebar, mypageSidebarParam } from '$lib/stores';
+	import { onDestroy } from 'svelte';
+	import MDViewer from '$lib/components/common/MDViewer.svelte';
+	export let data;
+
+	const activity = data.activityData;
+	const userStatus = data.userStatus;
+
+	$mypageSidebar = userStatus === 'super' ? Symbol.for('super') : Symbol.for('admin');
+	$mypageSidebarParam = activity.id;
+
+	onDestroy(() => {
+		$mypageSidebar = Symbol.for('default');
+	});
+</script>
+
+<ContentLayer title={activity.title} hasBack={true}>
+	<div class="w-full px-4">
+		<MDViewer mdTypeContent={activity.details} />
+	</div>
+</ContentLayer>
---
+++ src/routes/users/activities/admin/[id]/+page.ts
@@ -0,0 +1,37 @@
+import { user } from '$lib/stores';
+import { get } from 'svelte/store';
+import { supabase } from '$lib/supabase';
+
+const getActivityData = async (id: string) => {
+	const { data, error } = await supabase
+		.from('activities')
+		.select('*, activity_types("type", "type_kor"), images("storage_id")')
+		.match({ id });
+	return error ? [] : data[0];
+};
+
+const getUserStatus = async ({
+	user_id,
+	activity_id
+}: {
+	user_id: string | undefined;
+	activity_id: string;
+}) => {
+	if (!user_id) return 'logout';
+
+	const { data, error } = await supabase
+		.from('participants')
+		.select('status')
+		.match({ user_id, activity_id })
+		.single();
+
+	return error ? 'error' : data.status;
+};
+
+/** @type {import('./$types').PageLoad} */
+export async function load({ params, parent }) {
+	await parent();
+	const activityData = await getActivityData(params.id);
+	const userStatus = await getUserStatus({ user_id: get(user)?.id, activity_id: activityData.id });
+	return { activityData, userStatus };
+}
