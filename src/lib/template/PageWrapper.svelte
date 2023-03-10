b91627da6a96aa17dcfb96312e70930d3c44be0d
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-03-10 15:03:24 +0900

    [feat] 페이지 안에 컨텐츠 들어가는 영역 통일 위한 컴포넌트 구현

--- src/lib/template/PageWrapper.svelte
+++ src/lib/template/PageWrapper.svelte
@@ -2,6 +2,6 @@
 	import { fade } from 'svelte/transition';
 </script>
 
-<div class="w-full lg:p-10 py-14 px-6" in:fade={{ duration: 500 }}>
+<div class="flex flex-col w-full h-full lg:p-10 py-14 px-6" in:fade={{ duration: 500 }}>
 	<slot />
 </div>
