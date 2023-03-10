0fa039c49e33cc5a6bb8a51665f2108472aed89f
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-03-10 15:03:52 +0900

    [fix] 질문이 없는 지원서의 경우 처리

--- src/routes/applications/[id]/+page.svelte
+++ src/routes/applications/[id]/+page.svelte
@@ -34,12 +34,18 @@
 		<h1 class="text-xl font-bold">작성하신 내용</h1>
 	</div>
 	<div class="flex flex-col gap-8 mt-12">
-		{#each answers as answer, idx}
-			<div>
-				<h2 class="font-semibold">{idx + 1}. {answer.activity_forms.question}</h2>
-				<p class="min-h-[144px] p-2 mt-2 bg-gray-100 rounded">{answer.answer}</p>
+		{#if answers.length > 0}
+			{#each answers as answer, idx}
+				<div>
+					<h2 class="font-semibold">{idx + 1}. {answer.activity_forms.question}</h2>
+					<p class="min-h-[144px] p-2 mt-2 bg-gray-100 rounded">{answer.answer}</p>
+				</div>
+			{/each}
+		{:else}
+			<div class="flex justify-center items-center w-full h-36 rounded bg-gray-100 font-semibold">
+				질문 / 응답이 없습니다! 😊
 			</div>
-		{/each}
+		{/if}
 	</div>
 	<div class="flex flex-col">
 		<h2 class="text-xl font-bold mt-12">지원 결과</h2>
